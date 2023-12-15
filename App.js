67fd017fa7f35297c12d204a9410916efea70d4a
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-15 20:56:28 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 // App.js
 import React from 'react';
 import MenuBar from './MenuBar';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 const App = () => {
   return (
     <div className="app">
       <MenuBar />
+      <Home />
       {/* Add other components/content here */}
     </div>
   );
