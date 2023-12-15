65dbbfc2391c9816e005cc8c911fce8b4d07b175
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-15 09:05:39 +0700

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Add content specific to the home page */}
+    </div>
+  );
+};
+
+export default Home;
