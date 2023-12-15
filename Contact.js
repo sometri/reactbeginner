42dea6c40a5f61a537e45c876778ceb68cadc697
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-15 21:23:33 +0700

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,11 +1,25 @@
 // Contact.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Contact = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Simulate data fetching from an API
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      {/* Add content specific to the contact page */}
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Contact Us</h1>
+      {data ? (
+        <p>{data.title}</p>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 };
