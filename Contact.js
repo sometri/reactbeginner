43f445e82ae81dfc382a0b5cf9d339505688ea02
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-15 09:07:27 +0700

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      {/* Add content specific to the contact page */}
+    </div>
+  );
+};
+
+export default Contact;
