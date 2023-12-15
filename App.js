4ce9187b9938a9ed03c6b186b8b5332f7d1ae007
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-12-15 20:52:23 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import MenuBar from './MenuBar';
+
+const App = () => {
+  return (
+    <div className="app">
+      <MenuBar />
+      {/* Add other components/content here */}
+    </div>
+  );
+};
+
+export default App;
